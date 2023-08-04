9e616f8b463bd201f16321775d7dfb269676d3c2
Greg Valijan <greg.valijan@gmail.com>
2023-08-04 13:21:03 -0700

    uibuild

--- phonebook/backend/index.js
+++ phonebook/backend/index.js
@@ -6,6 +6,8 @@ app.use(express.json())
 const cors = require('cors')
 app.use(cors())
 
+app.use(express.static('build'))
+
 const morgan = require('morgan')
 morgan.token('object', function (req, res) { return JSON.stringify(req.body)})
 app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :object`));
