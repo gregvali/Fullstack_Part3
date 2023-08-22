1a3c456566d6d3c08aa5e8ed1bb6c7848c02a899
Greg Valijan <greg.valijan@gmail.com>
2023-08-22 12:48:14 -0700

    uibuild

--- phonebook/backend/index.js
+++ phonebook/backend/index.js
@@ -1,12 +1,10 @@
 require('dotenv').config()
 const express = require('express')
 const Person = require('./models/person')
-const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(cors())
 app.use(express.static('build'))
 
 morgan.token('object', function (req, res) { return JSON.stringify(req.body)})
@@ -35,17 +33,24 @@ app.get('/api/phonebook', (request, response) => {
   })
 })
 
-app.get('/api/phonebook/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+app.get('/api/phonebook/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/phonebook/:id', (request, response) => {
-  const id = Number(request.params.id)
-  numbers = numbers.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/phonebook', (request, response) => {
@@ -67,6 +72,39 @@ app.post('/api/phonebook', (request, response) => {
     response.json(savedPerson)
   })
 })
+
+app.put('/api/phonebook/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
